ea58e8e746f8162b4fec37eccc995adbe9de5e75
Sameer joshi <35561477+joshiSameer1111@users.noreply.github.com>
2026-04-12 17:47:11 -0500

    completed challenge with async await

---
+++ fetching_with_async_await/index.js
@@ -0,0 +1,18 @@
+/*
+Challenge:
+    1. Make a fetch request to the "Bored" API: 
+         Base URL: https://apis.scrimba.com/bored/api
+         Endpoint: /activity
+    2. Log an object containing an activity suggestion 
+       to the console.
+    ⚠️ Make sure you use the async/await method!
+    🛟 hint.md for help!
+*/
+
+async function getActivity() {
+  const response = await fetch("https://apis.scrimba.com/bored/api/activity")
+  const data = await response.json()
+  console.log(data)
+}
+
+getActivity()
