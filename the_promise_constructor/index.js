8a0cd09a359c850b35c0553b11cdb6d1ce2007dc
Sameer joshi <35561477+joshiSameer1111@users.noreply.github.com>
2026-04-12 19:44:06 -0500

    the promise constructor challenge completed

---
+++ the_promise_constructor/index.js
@@ -0,0 +1,29 @@
+/*
+Challenge:
+1. Get the promise working so it returns “Operation successful!” 
+   if it resolves and "Operation failed." if it rejects. 
+*/
+const promise = () => {
+    return new Promise((resolve, reject) => {
+        const success = Math.random() > 0.5
+
+        if (success) {
+            resolve("Operation successful!")
+        } else {
+            reject("Operation failed.")
+        }
+    })
+}
+
+// promise.then(response => console.log(response))
+
+async function runPromise() {
+    try {
+        const response = await promise()
+        console.log(response)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+runPromise()
