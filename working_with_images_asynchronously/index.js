b2feefb74e6906b0659fc28fb0bb71170dbb0f04
Sameer joshi <35561477+joshiSameer1111@users.noreply.github.com>
2026-04-12 19:51:59 -0500

    Working With Images Asynchronously challenge completed

---
+++ working_with_images_asynchronously/index.js
@@ -0,0 +1,22 @@
+const image = new Image()
+image.src = "http://..."
+
+console.log(image)
+
+/*
+Challenge:
+1. Create two event listeners. One should listen 
+   out for the image loading and log “Image has 
+   loaded”. The other should listen for an error 
+   and log “Image has NOT loaded”.
+*/
+
+image.addEventListener("load", () => {
+    console.log("Image has loaded")
+})
+
+image.addEventListener("error", () => {
+    console.log("Image has NOT loaded")
+})
+
+console.log(image)
