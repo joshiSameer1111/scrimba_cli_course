935cd032cdd4e3d60094a593399fdbc422003350
Sameer joshi <35561477+joshiSameer1111@users.noreply.github.com>
2026-04-12 19:27:04 -0500

    Api request the bodu challenge 1 completed

---
+++ API_requests_the_body/index.js
@@ -0,0 +1,49 @@
+try {
+    const response = await fetch('https://apis.scrimba.com/jsonplaceholder/posts', {method: 'POST'})
+    if (!response.ok) {
+        throw new Error('There was a problem with the API')
+    }
+    const data = await response.json()
+    console.log(data)
+} catch(err) {
+    console.log(err)
+}  
+
+/*
+Challenge:
+1. Using the code snippet in the slide, add a body 
+   property to the object we are passing with the 
+   fetch request. I want you to create a new post 
+   with the title “Holiday Nightmares” and the body 
+   “When I was kidnapped in Scotland…” 
+   
+   In the console, you should see and object with an 
+   ID, for example: {id: 101}
+*/
+
+try {
+    const response = await fetch(
+            'https://apis.scrimba.com/jsonplaceholder/posts',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+                body: JSON.stringify({
+                title: "Holiday Nightmares",
+                body: "When I was kidnapped in Scotland…",
+                id: 10
+            })
+        }
+    )
+
+    if (!response.ok) {
+        throw new Error('There was a problem with the API')
+    }
+
+    const data = await response.json()
+    console.log(data)
+
+} catch (err) {
+    console.log(err)
+}
