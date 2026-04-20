e2010c4fedc762b221dbdf52c868cb1d8322b8f3
Sameer joshi <35561477+joshiSameer1111@users.noreply.github.com>
2026-04-20 11:16:18 -0500

    modularise code 2 challenge 1st

---
+++ modularise_code_2/server.js
@@ -0,0 +1,35 @@
+import http from 'node:http'
+import { getDataFromDB } from './database/db.js'
+import { sendJSONResponse } from './utils/sendJSONResponse.js'
+
+const PORT = 8000
+
+const server = http.createServer(async (req, res) => {
+  const destinations = await getDataFromDB()
+
+  if (req.url === '/api' && req.method === 'GET') {
+    sendJSONResponse(res, 200, destinations)
+
+  } else if (req.url.startsWith('/api/continent') && req.method === 'GET') {
+    const continent = req.url.split('/').pop()
+    const filteredData = destinations.filter(destination =>
+      destination.continent.toLowerCase() === continent.toLowerCase()
+    )
+    sendJSONResponse(res, 200, filteredData)
+
+  } else if (req.url.startsWith('/api/country') && req.method === 'GET') {
+    const country = req.url.split('/').pop()
+    const filteredData = destinations.filter(destination =>
+      destination.country.toLowerCase() === country.toLowerCase()
+    )
+    sendJSONResponse(res, 200, filteredData)
+
+  } else {
+    sendJSONResponse(res, 404, {
+      error: "not found",
+      message: "The requested route does not exist"
+    })
+  }
+})
+
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
