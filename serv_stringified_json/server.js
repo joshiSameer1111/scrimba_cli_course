497358754929a0b4e8c18b51da62dd3d0e77bb58
Sameer joshi <35561477+joshiSameer1111@users.noreply.github.com>
2026-04-20 10:50:06 -0500

    serv stringified json challenge

---
+++ serv_stringified_json/server.js
@@ -0,0 +1,17 @@
+import http from 'node:http'
+import { getDataFromDB } from './database/db.js'
+ 
+const PORT = 8000
+
+const server = http.createServer(async (req, res) => {
+
+  const destinations = await getDataFromDB()
+
+  if (req.url === '/api' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(destinations))
+  }
+
+})
+
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
