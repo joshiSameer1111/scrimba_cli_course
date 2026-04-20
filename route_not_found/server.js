9083f3b5f39a62acd35e546ca0051100578ba390
Sameer joshi <35561477+joshiSameer1111@users.noreply.github.com>
2026-04-20 11:02:21 -0500

    Route not found challenge

---
+++ route_not_found/server.js
@@ -0,0 +1,23 @@
+import http from 'node:http'
+import { getDataFromDB } from './database/db.js'
+
+const PORT = 8000
+
+const server = http.createServer(async (req, res) => {
+  const destinations = await getDataFromDB()
+
+  if (req.url === '/api' && req.method === 'GET') {
+    res.setHeader('Content-Type', 'application/json')
+    res.statusCode = 200
+    res.end(JSON.stringify(destinations))
+  } else {
+    res.statusCode = 404
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify({
+      error: "not found",
+      message: "The requested route does not exist"
+    }))
+  }
+})
+
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
