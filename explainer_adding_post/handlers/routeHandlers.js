bd8ced553b99c24eca915c69e77084d1e9a12e7b
Sameer joshi <35561477+joshiSameer1111@users.noreply.github.com>
2026-04-27 09:55:26 -0500

    adding POST challenge

---
+++ explainer_adding_post/handlers/routeHandlers.js
@@ -0,0 +1,14 @@
+import { getData } from '../utils/getData.js'
+import { sendResponse } from '../utils/sendResponse.js'
+
+export async function handleGet(req, res) {
+  const data = await getData()
+  const content = JSON.stringify(data)
+  sendResponse(res, 200, 'application/json', content)
+}
+
+// handlePost
+
+export async function handlePost(req, res) {
+  console.log('POST request received')
+}
---
+++ explainer_adding_post/server.js
@@ -0,0 +1,28 @@
+import http from 'node:http'
+import { serveStatic } from './utils/serveStatic.js'
+import { handleGet, handlePost } from './handlers/routeHandlers.js'
+
+const PORT = 8000
+
+const __dirname = import.meta.dirname
+
+const server = http.createServer(async (req, res) => {
+
+    if (req.url === '/api') {
+
+        if (req.method === 'GET') {
+            return await handleGet(req, res)
+        }
+
+        if (req.method === 'POST') {
+            return await handlePost(req, res)
+        }
+
+    }
+
+    if (!req.url.startsWith('/api')) {
+        return await serveStatic(req, res, __dirname)
+    }
+})
+
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
