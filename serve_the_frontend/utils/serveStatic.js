c5626dcf193406a2d2430c0b589407b93f4236a6
Sameer joshi <35561477+joshiSameer1111@users.noreply.github.com>
2026-04-27 09:26:26 -0500

    serve the frontend 1st challenge

---
+++ serve_the_frontend/utils/serveStatic.js
@@ -0,0 +1,26 @@
+import path from 'node:path'
+import fs from 'node:fs/promises'
+import { sendResponse } from './sendResponse.js'
+import { getContentType } from './getContentType.js'
+
+export async function serveStatic(req, res, baseDir) {
+
+  const filePath = path.join(
+    baseDir,
+    'public',
+    req.url === '/' ? 'index.html' : req.url
+  )
+
+  try {
+    const content = await fs.readFile(filePath)
+
+    const ext = path.extname(filePath)
+    const contentType = getContentType(ext)
+
+    sendResponse(res, 200, contentType, content)
+
+  } catch (err) {
+    console.log(err)
+  }
+
+}
