ab434e28f2343db05f45ff7228e6e966acbcca34
Sameer joshi <35561477+joshiSameer1111@users.noreply.github.com>
2026-04-27 10:44:22 -0500

    add an event emitter challenge

---
+++ add_an_event_emitter/events/sightingEvents.js
@@ -0,0 +1,8 @@
+import { EventEmitter } from 'node:events'
+import { createAlert } from '../utils/createAlert.js'
+
+export const sightingEvents = new EventEmitter()
+
+sightingEvents.on('sighting-added', (data) => {
+  createAlert(data)
+})
